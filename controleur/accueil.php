e92213556465ac6bfb3ebcc0307a39d98c548de1
Robocoq <105200919+Robocoq@users.noreply.github.com>
2022-11-14 16:49:03 +0100

    navbar FINI

---
+++ controleur/accueil.php
@@ -0,0 +1,10 @@
+<?php
+
+if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__) {
+    $racine ="..";
+}
+
+$titre = "poele";
+include "$racine/vue/vueFooter.php";
+
+?>
---
+++ vue/vueFooter.php
@@ -0,0 +1,20 @@
+</div>
+<style type="text/css">
+    @import url("css/footer.css");
+</style>
+
+<div class="fondFooter">
+    <div class="infoMarie">
+        <p>
+        Marie-Team
+        <br>
+        36110 Indre06 
+        <br>
+        00 00 00 00
+        </p>
+    </div>
+    <div  class="mailMarie">
+        <p>contact@marieteam.com</p>
+    </div>
+    <img src="images/LogoMarie-Team.png" class="logoFooter" alt="logoFooter">
+</div>
