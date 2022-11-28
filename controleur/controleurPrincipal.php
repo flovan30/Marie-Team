f8a99390aecfaad4d92371c5cc19ee37e0b77d12
flovan30 <florianvanreust@gmail.com>
2022-11-28 13:52:24 +0100

    bonne redirection

--- controleur/controleurPrincipal.php
+++ controleur/controleurPrincipal.php
@@ -1,18 +1,18 @@
 <?php
 
-function controleurPrincipal($action){
+function controleurPrincipal($action)
+{
     $lesActions = array();
     $lesActions["defaut"] = "accueil.php";
-    $lesActions["reservation"] = "reservation.php";
+    $lesActions["destination"] = "reservation.php";
     $lesActions["tarifs"] = "tarif.php";
     $lesActions["connexion"] = "connexion.php";
     $lesActions["inscription"] = "inscription.php";
 
 
-    if (array_key_exists ( $action , $lesActions )){
+    if (array_key_exists($action, $lesActions)) {
         return $lesActions[$action];
-    }
-    else{
+    } else {
         return $lesActions["defaut"];
     }
 }
---
+++ controleur/reservation.php
@@ -0,0 +1,10 @@
+<?php
+
+if ($_SERVER["SCRIPT_FILENAME"] == __FILE__) {
+    $racine = "..";
+}
+
+include "$racine/vue/vueNavbar.php";
+include "$racine/vue/vueFooter.php";
+
+?>
