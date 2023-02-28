a38363234d67849b8a7014980b935a29c6ee448c
flovan30 <florianvanreust@gmail.com>
2023-02-28 21:34:16 +0100

    avancement destination

--- controleur/reservation.php
+++ controleur/reservation.php
@@ -4,7 +4,12 @@ if ($_SERVER["SCRIPT_FILENAME"] == __FILE__) {
     $racine = "..";
 }
 
+include_once "$racine/modele/bd.reservation.inc.php";
+
+$listeSecteur = getSecteur();
+
 include "$racine/vue/vueNavbar.php";
+include "$racine/vue/vueAffichageReservation.php";
 include "$racine/vue/vueFooter.php";
 
 ?>
