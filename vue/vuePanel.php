14c198b9210c00e9024c97c2223ea9e74248f7cf
Estebannnnnnn <esteban.lhoste7@hotmail.com>
2023-03-27 17:02:27 +0200

    Fix admin nb passagers

--- vue/vuePanel.php
+++ vue/vuePanel.php
@@ -5,17 +5,7 @@
     </section>
     
     <section id=nbReservationMoy>
-        z
-        ef
-        rezg
-        rze
-        rze
-        ez
-        gz
-        gzr
-        gzr
-        gezr
-        fhziehfeaifjeaopfkaeofkjze
+        
     </section>
     
     <section id=chiffreAffaire>
@@ -23,6 +13,41 @@
     </section>
     
     <section id=nbPersonneTransp>
+        <?php
+
+        $lastWeek = nbPassagersForLastWeek();
+        echo '<div>Nombre de passagers ayant reservé et ayant voyagé cette dernière semaine :<div>';
+        if ($lastWeek[0]['totalPassagers'] == 0) {
+            echo '0';
+        } else {
+            echo $lastWeek[0]['totalPassagers'];
+        }
         
+
+        $lastMonth = nbPassagersForLastMonth();
+        echo '<div>Nombre de passagers ayant reservé et ayant voyagé ce dernier mois :<div>';
+        if ($lastMonth[0]['totalPassagers'] == 0) {
+            echo '0';   
+        } else {
+            echo $lastMonth[0]['totalPassagers'];
+        }
+
+        $lastYear = nbPassagersForLastYear();
+        echo '<div>Nombre de passagers ayant reservé et ayant voyagé cette dernière année :<div>';
+        if ($lastYear[0]['totalPassagers'] == 0) {
+            echo '0';   
+        } else {
+            echo $lastYear[0]['totalPassagers'];
+        }
+
+        $everytime = nbPassagersForEverytime();
+        echo '<div>Nombre de passagers ayant reservé et ayant voyagé depuis le début de Marie Team :<div>';
+        if ($everytime[0]['totalPassagers'] == 0) {
+            echo '0';   
+        } else {
+            echo $everytime[0]['totalPassagers'];
+        }
+
+        ?>
     </section>
 </div>
