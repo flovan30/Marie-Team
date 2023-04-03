b4155ecb8d1d570789aa73729d30c4ae9fe4ea2a
Robocoq <pierremonnier19@gmail.com>
2023-04-03 14:41:47 +0200

    Rectification Panel

--- vue/vuePanel.php
+++ vue/vuePanel.php
@@ -83,22 +83,17 @@
 </section>
 
 <section id=nbPersonneTransp>
+
         <?php
-
-
         $lastWeek = nbPassagersForLastWeek();
-        if ($lastWeek[0]['totalPassagers'] == 0) {
-            echo '0';
-        } else {
-            echo $lastWeekPassager[0]['totalPassagers'];
-        }
-        
         $lastMonth = nbPassagersForLastMonth();
         $lastYear = nbPassagersForLastYear();
         $everytime = nbPassagersForEverytime();
         $evertimeByCategorie = nbPassagersForEverytimeByCodeCategorie();
-        echo '<div>Nombre de passagers ayant reservé et ayant voyagé selon la catégorie :<div>';
-        echo '
+        ?>
+
+        <div>Nombre de passagers ayant reservé et ayant voyagé selon la catégorie :<div>
+        
         <table>
             <tr>
                 <td></td>
@@ -129,33 +124,42 @@
                 <td></td>
             </tr>
             <tr>
-                <td>Total';
-                echo '</td><td>';
-
-                echo '</td><td>';
-                    if ($lastMonth[0]['totalPassagers'] == 0) {
-                        echo '0';   
-                    } else {
-                        echo $lastMonth[0]['totalPassagers'];
-                    }
-                echo '</td><td>';
-                    if($lastYear[0]['totalPassagers'] == 0) {
-                        echo '0';   
-                    } else {
-                        echo $lastYear[0]['totalPassagers'];
-                    }
-                echo '</td><td>';
-                    if ($everytime[0]['totalPassagers'] == 0) {
-                        echo '0';   
-                    } else {
-                        echo $everytime[0]['totalPassagers'];
-                    }
-                echo '</td>  
+                <td>Total
+                </td><td>
+                    <?php
+                        if ($lastWeek[0]['totalPassagers'] == 0) {
+                            echo '0';
+                        } else {
+                            echo $lastWeekPassager[0]['totalPassagers'];
+                        }
+                    ?>
+                </td><td>
+                    <?php
+                        if ($lastMonth[0]['totalPassagers'] == 0) {
+                            echo '0';   
+                        } else {
+                            echo $lastMonth[0]['totalPassagers'];
+                        }
+                    ?>
+                </td><td>
+                    <?php
+                        if($lastYear[0]['totalPassagers'] == 0) {
+                            echo '0';   
+                        } else {
+                            echo $lastYear[0]['totalPassagers'];
+                        }
+                    ?>
+                </td><td>
+                    <?php
+                        if ($everytime[0]['totalPassagers'] == 0) {
+                            echo '0';   
+                        } else {
+                            echo $everytime[0]['totalPassagers'];
+                        }
+                    ?>
+                </td>  
             </tr>
       </table>
-      ';
-
-        ?>
 </section>
 
 </div>
