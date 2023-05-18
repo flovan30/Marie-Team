032f7dde3cb62356c0505615891558b6db2c0368
Estebannnnnnn <esteban.lhoste7@hotmail.com>
2023-05-19 00:18:52 +0200

    Tableau sans CSS let'ssss gooo

--- modele/nbPassager.php
+++ modele/nbPassager.php
@@ -12,11 +12,7 @@ function nbPassagersForLastWeek()
         AND t.dateTraversee BETWEEN DATE_SUB(NOW(), INTERVAL 1 WEEK) AND NOW();");
         $req->execute();
         
-        $ligne = $req->fetch(PDO::FETCH_ASSOC);
-        while ($ligne) {
-            $resultat[] = $ligne;
-            $ligne = $req->fetch(PDO::FETCH_ASSOC);
-        }
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
     } catch (PDOException $e) {
         print "Erreur !: " . $e->getMessage();
         die();
@@ -34,11 +30,7 @@ function nbPassagersForLastMonth()
         AND t.dateTraversee BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW();");
         $req->execute();
         
-        $ligne = $req->fetch(PDO::FETCH_ASSOC);
-        while ($ligne) {
-            $resultat[] = $ligne;
-            $ligne = $req->fetch(PDO::FETCH_ASSOC);
-        }
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
     } catch (PDOException $e) {
         print "Erreur !: " . $e->getMessage();
         die();
@@ -56,11 +48,7 @@ function nbPassagersForLastYear()
         AND t.dateTraversee BETWEEN DATE_SUB(NOW(), INTERVAL 1 YEAR) AND NOW();");
         $req->execute();
         
-        $ligne = $req->fetch(PDO::FETCH_ASSOC);
-        while ($ligne) {
-            $resultat[] = $ligne;
-            $ligne = $req->fetch(PDO::FETCH_ASSOC);
-        }
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
     } catch (PDOException $e) {
         print "Erreur !: " . $e->getMessage();
         die();
@@ -78,11 +66,7 @@ function nbPassagersForEverytime()
         AND t.dateTraversee BETWEEN DATE_SUB(NOW(), INTERVAL 1000 YEAR) AND NOW();");
         $req->execute();
         
-        $ligne = $req->fetch(PDO::FETCH_ASSOC);
-        while ($ligne) {
-            $resultat[] = $ligne;
-            $ligne = $req->fetch(PDO::FETCH_ASSOC);
-        }
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
     } catch (PDOException $e) {
         print "Erreur !: " . $e->getMessage();
         die();
@@ -90,22 +74,227 @@ function nbPassagersForEverytime()
     return $resultat;
 }
 
-function nbPassagersForEverytimeByCodeCategorie()
+function nbPassagersLastWeekByCodeCategorieA()
 {
      try {
         $cnx = connexionPDO();
         $req = $cnx->prepare("select sum(e.quantité) AS totalPassagers, e.codeCategorie
         FROM enregistrer e, reservation r, traversee t
         WHERE e.numReservation = r.numReservation AND r.numTraversee = t.numTraversee
-        AND t.dateTraversee BETWEEN DATE_SUB(NOW(), INTERVAL 800 DAY) AND NOW()
-        GROUP BY e.codeCategorie;");
+        AND codeCategorie = 'A'
+        AND t.dateTraversee BETWEEN DATE_SUB(NOW(), INTERVAL 1 WEEK) AND NOW();");
+        $req->execute();    
+        
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
+function nbPassagersLastWeekByCodeCategorieB()
+{
+     try {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select sum(e.quantité) AS totalPassagers, e.codeCategorie
+        FROM enregistrer e, reservation r, traversee t
+        WHERE e.numReservation = r.numReservation AND r.numTraversee = t.numTraversee
+        AND codeCategorie = 'B'
+        AND t.dateTraversee BETWEEN DATE_SUB(NOW(), INTERVAL 1 WEEK) AND NOW();");
+        $req->execute();    
+        
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
+function nbPassagersLastWeekByCodeCategorieC()
+{
+     try {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select sum(e.quantité) AS totalPassagers, e.codeCategorie
+        FROM enregistrer e, reservation r, traversee t
+        WHERE e.numReservation = r.numReservation AND r.numTraversee = t.numTraversee
+        AND codeCategorie = 'C'
+        AND t.dateTraversee BETWEEN DATE_SUB(NOW(), INTERVAL 1 WEEK) AND NOW();");
+        $req->execute();    
+        
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
+function nbPassagersLastMonthByCodeCategorieA()
+{
+     try {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select sum(e.quantité) AS totalPassagers, e.codeCategorie
+        FROM enregistrer e, reservation r, traversee t
+        WHERE e.numReservation = r.numReservation AND r.numTraversee = t.numTraversee
+        AND codeCategorie = 'A'
+        AND t.dateTraversee BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW();");
         $req->execute();
         
-        $ligne = $req->fetch(PDO::FETCH_ASSOC);
-        while ($ligne) {
-            $resultat[] = $ligne;
-            $ligne = $req->fetch(PDO::FETCH_ASSOC);
-        }
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
+function nbPassagersLastMonthByCodeCategorieB()
+{
+     try {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select sum(e.quantité) AS totalPassagers, e.codeCategorie
+        FROM enregistrer e, reservation r, traversee t
+        WHERE e.numReservation = r.numReservation AND r.numTraversee = t.numTraversee
+        AND codeCategorie = 'B'
+        AND t.dateTraversee BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW();");
+        $req->execute();
+        
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
+function nbPassagersLastMonthByCodeCategorieC()
+{
+     try {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select sum(e.quantité) AS totalPassagers, e.codeCategorie
+        FROM enregistrer e, reservation r, traversee t
+        WHERE e.numReservation = r.numReservation AND r.numTraversee = t.numTraversee
+        AND codeCategorie = 'C'
+        AND t.dateTraversee BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW();");
+        $req->execute();
+        
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
+function nbPassagersLastYearByCodeCategorieA()
+{
+     try {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select sum(e.quantité) AS totalPassagers, e.codeCategorie
+        FROM enregistrer e, reservation r, traversee t
+        WHERE e.numReservation = r.numReservation AND r.numTraversee = t.numTraversee
+        AND codeCategorie = 'A'
+        AND t.dateTraversee BETWEEN DATE_SUB(NOW(), INTERVAL 1 YEAR) AND NOW();");
+        $req->execute();
+        
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
+function nbPassagersLastYearByCodeCategorieB()
+{
+     try {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select sum(e.quantité) AS totalPassagers, e.codeCategorie
+        FROM enregistrer e, reservation r, traversee t
+        WHERE e.numReservation = r.numReservation AND r.numTraversee = t.numTraversee
+        AND codeCategorie = 'B'
+        AND t.dateTraversee BETWEEN DATE_SUB(NOW(), INTERVAL 1 YEAR) AND NOW();");
+        $req->execute();
+        
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
+function nbPassagersLastYearByCodeCategorieC()
+{
+     try {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select sum(e.quantité) AS totalPassagers, e.codeCategorie
+        FROM enregistrer e, reservation r, traversee t
+        WHERE e.numReservation = r.numReservation AND r.numTraversee = t.numTraversee
+        AND codeCategorie = 'C'
+        AND t.dateTraversee BETWEEN DATE_SUB(NOW(), INTERVAL 1 YEAR) AND NOW();");
+        $req->execute();
+        
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
+function nbPassagersForEverytimeByCodeCategorieA()
+{
+     try {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select sum(e.quantité) AS totalPassagers, e.codeCategorie
+        FROM enregistrer e, reservation r, traversee t
+        WHERE e.numReservation = r.numReservation AND r.numTraversee = t.numTraversee
+        AND codeCategorie = 'A'
+        AND t.dateTraversee BETWEEN DATE_SUB(NOW(), INTERVAL 800 DAY) AND NOW();");
+        $req->execute();
+        
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
+function nbPassagersForEverytimeByCodeCategorieB()
+{
+     try {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select sum(e.quantité) AS totalPassagers, e.codeCategorie
+        FROM enregistrer e, reservation r, traversee t
+        WHERE e.numReservation = r.numReservation AND r.numTraversee = t.numTraversee
+        AND codeCategorie = 'B'
+        AND t.dateTraversee BETWEEN DATE_SUB(NOW(), INTERVAL 800 DAY) AND NOW();");
+        $req->execute();
+        
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage();
+        die();
+    }
+    return $resultat;
+}
+
+function nbPassagersForEverytimeByCodeCategorieC()
+{
+     try {
+        $cnx = connexionPDO();
+        $req = $cnx->prepare("select sum(e.quantité) AS totalPassagers, e.codeCategorie
+        FROM enregistrer e, reservation r, traversee t
+        WHERE e.numReservation = r.numReservation AND r.numTraversee = t.numTraversee
+        AND codeCategorie = 'C'
+        AND t.dateTraversee BETWEEN DATE_SUB(NOW(), INTERVAL 800 DAY) AND NOW();");
+        $req->execute();
+        
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
     } catch (PDOException $e) {
         print "Erreur !: " . $e->getMessage();
         die();
